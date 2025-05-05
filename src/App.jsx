e512beed2a03b4c39ebcc16f1f12c3ca6f3b5b84
Farrukh(Fara) Dilshodov <64746228+faradilshodov@users.noreply.github.com>
2025-05-05 10:44:00 -0400

    Intro setup (#3)
    * setting up categories
    
    * add sass and update dependencies
    
    * Update directory page with directory and category item components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ]
+    const categories = [
+        {
+            "id": 1,
+            "title": "Hats",
+            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+        },
+        {
+            "id": 2,
+            "title": "Jackets",
+            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+        },
+        {
+            "id": 3,
+            "title": "Sneakers",
+            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+        },
+        {
+            "id": 4,
+            "title": "Womens",
+            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+        },
+        {
+            "id": 5,
+            "title": "Mens",
+            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+        },
+    ]
 
-  return (
-    <>
-      <div className="categories-container">
-        {categories.map(({ title, id }) => (
-          <div key={id} className="category-container">
-            <div className="background-image" />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+    return (
+        <Directory categories={categories} />
+    )
 }
 
 export default App
