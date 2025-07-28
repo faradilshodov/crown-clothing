6887197760c1a61dc2501e2263d3ec34d5ae452c
Farrukh(Fara) Dilshodov <64746228+faradilshodov@users.noreply.github.com>
2025-07-28 12:00:35 -0400

    Authentication (#8)
    * Add firebase package and sign in route
    
    * Add authenticating with firebase
    
    * Added storing logged in users in firebase db
    
    * Added signing up with email and password
    
    * Added generalized form input component and styling
    
    * Add reusable Button component and style sign-up form
    
    * Refactor sign-in flow and add SignInForm component
    
    * Improve authentication UI and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router"
 import Home from "./routes/home/home.component"
 import Navigation from "./routes/navigation/navigation.component"
+import Authentication from "./routes/authentication/authentication.component"
 
 const Shop = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="/" element={<Navigation />}>
                 <Route index element={<Home />} />
                 <Route path="shop" element={<Shop />} />
+                <Route path="auth" element={<Authentication />} />
             </Route>
         </Routes>
     )
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,68 @@
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3X_G9oSpyoyy0lWBBBASdXyd6V44aZ6s",
+    authDomain: "crown-clothing-db-e6922.firebaseapp.com",
+    projectId: "crown-clothing-db-e6922",
+    storageBucket: "crown-clothing-db-e6922.firebasestorage.app",
+    messagingSenderId: "184264612555",
+    appId: "1:184264612555:web:106f1d9f0cfeb4270ad94e",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+    prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+    signInWithPopup(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if (!userAuth) return;
+
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation
+            });
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
+    }
+    return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+};
