c1e5fcba0669ec98cf222f16c80b1bc5d2cf0e57
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-07-29 10:30:48 -0400

    Add user context for authentication state management

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Outlet, Link } from 'react-router';
 import  CrwnLogo from '../../assets/crown.svg?react';
+import { UserContext } from '../../contexts/user.context.jsx';
 import './navigation.styles.scss';
+
 const Navigation = () => {
+    const { currentUser } = useContext(UserContext);
 
     return (
         <>
