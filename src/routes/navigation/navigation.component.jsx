211db033e3aaba1d5d559b31cdd70b73f1afd4af
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-07-29 10:46:27 -0400

    Add sign out functionality and update user context
    Implemented sign out logic in navigation and exposed signOutUser utility. Updated sign-up form to set current user on registration and navigation to clear user context on sign out.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,10 +2,16 @@ import { useContext } from 'react';
 import { Outlet, Link } from 'react-router';
 import  CrwnLogo from '../../assets/crown.svg?react';
 import { UserContext } from '../../contexts/user.context.jsx';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    }
 
     return (
         <>
@@ -15,7 +21,13 @@ const Navigation = () => {
                 </Link>
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>SHOP</Link>
-                    <Link className='nav-link' to='/auth'>SIGN IN</Link>
+                    {
+                        currentUser ? (
+                            <span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>
+                        ) : (
+                            <Link className='nav-link' to='/auth'>SIGN IN</Link>
+                        )
+                    }
                 </div>
             </div>
             <Outlet />
