7c2763b2c518faa6ffa3c43d0dd639640d45f547
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-07-31 14:08:14 -0400

    Add remove item from cart functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss';
 
 const Checkout = () => {
-    const { cartItems, addItemToCart } = useContext(CartContext);
+    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
 
     return (
         <div>
@@ -18,8 +18,8 @@ const Checkout = () => {
                             <div key={id}>
                                 <h2>{name}</h2>
                                 <span>{quantity}</span>
-                                <span>decrement</span>
-                                <br />  
+                                <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
+                                <br />
                                 <span onClick={() => addItemToCart(cartItem)}>increment</span>
                             </div>
                         );
